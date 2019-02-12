336e352226a974802561ae4cceb4018edb09865b
Ralph W. Crosby <ralph.crosby@navy.mil>
2019-02-12 07:14:20 -0800

    Added create code

--- python/app.py
+++ python/app.py
@@ -11,7 +11,7 @@ crosbyrw@cofc.edu
 
 # **************************************************
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, g
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
@@ -23,7 +23,8 @@ db = client.GradeBook
 
 @app.route('/', methods=['GET','POST'])
 def students():
-    '''Connect to the database and display the contents of the collection
+    '''
+    Connect to the database and display the contents of the collection
     '''
 
     if request.method == 'POST':
@@ -32,9 +33,8 @@ def students():
         cmd = request.form[id]
         
         if cmd == 'add':
-            
-            msg='add a record'
-            app.logger.info('add')
+
+            return redirect(url_for("create"))
             
         elif cmd == 'delete':
 
@@ -49,10 +49,10 @@ def students():
     else:
         msg = ''    
 
-    students = db.students.find()
-
+    students = list(db.students.find())
+   
     try:
-        n_grades = len(students[0].grades)
+        n_grades = max(len(s['grades']) for s in students)
     except:
         n_grades = 1
 
@@ -60,7 +60,29 @@ def students():
                            students=students,
                            msg=msg,
                            n_grades=n_grades)
-        
+
+# **************************************************
+
+@app.route('/create', methods=['GET','POST'])
+def create():
+    """
+    Create a new student record
+    """
+    
+    if request.method == 'POST':
+
+        name = request.form["name"]
+        grades_text = request.form["grades"]
+
+        grades = [float(grade) for grade in grades_text.split(',')]
+ 
+        db.students.insert(dict(name=name, grades=grades))
+
+        return redirect(url_for("students"))
+
+    return render_template('create.html')
+
+
 # **************************************************
 
 if __name__ == '__main__':
